a8a145c329b8cc178f214ac11c71ceb1ba89d2f1
Oleg Domanov <odomanov@yandex.ru>
2026-01-09 13:59:06 +0700

    Modified   SigmaProgramming/Ax_equality_of_capacity.lean Modified   SigmaProgramming/Axioms.lean

--- SigmaProgramming/Axioms.lean
+++ SigmaProgramming/Axioms.lean
@@ -7,30 +7,24 @@ namespace GES
 variable [m : SModel]
 open S SM
 
-theorem Ax_empty_list  : ¬ (α ∈ list nil) := nofun
+theorem Ax_empty_list  : ∀ α, α ∉ nil := nofun
 theorem Ax_uniqueness : cons αs α = cons βs β → αs = βs ∧ α = β := by
-  intro p
-  injection p
-  constructor <;> assumption
+  intro p; injection p; constructor <;> assumption
 theorem Ax_list1 : tail (cons αs α) = αs := by cases α <;> trivial
 theorem Ax_list2 : head (cons αs α) = α := by cases α <;> trivial
 theorem Ax_list3 (ne : αs ≠ nil) : (cons (tail αs) (head αs)) = αs := by
   cases αs <;> trivial
 theorem Ax_list4 : tail nil = nil := by trivial
 theorem Ax_list5 : head nil = list nil := by trivial
-theorem Ax_list6 : cons (conc a b) c = conc a (cons b c) := by
-  cases a <;> cases b <;> cases c <;> trivial
-theorem Ax_list7 : ∀ α β γ, conc (conc α β) γ = conc α (conc β γ) := by
-  intros _ _ γ
+theorem Ax_list6 : cons (conc α β) γ = conc α (cons β γ) := by simp_all
+theorem Ax_list7 : conc (conc α β) γ = conc α (conc β γ) := by
   induction γ with
   | nil => rfl
-  | cons _ hd ih =>
-    rw [conc,conc,conc]
-    exact congrFun (congrArg cons ih) hd
-theorem Ax_list8 : ∀ αs, conc nil αs = αs
+  | cons _ _ _ => simp_all
+theorem Ax_list8 : ∀ α, conc nil α = α
   | .nil => rfl
-  | .cons βs β => conc_nil β βs (Ax_list8 βs)
-theorem Ax_list9 : conc αs nil = αs := rfl
+  | .cons β _ => by simp_all; cases β <;> apply Ax_list8
+theorem Ax_list9 : conc α nil = α := rfl
 theorem Ax_induction {Φ : S → Prop} :   --    элиминаторы не стандартные!
     Φ nil
   → (∀ α δ, (Φ α → Φ (cons α δ)))
