fd75aa0a227786a55bb86fac962684c5fedd3c3d
Oleg Domanov <odomanov@yandex.ru>
2026-01-13 16:33:57 +0700

    Modified   SigmaProgramming/MemWF.lean Missing    SigmaProgramming/MemWF3.lean

--- SigmaProgramming/MemWF.lean
+++ SigmaProgramming/MemWF.lean
@@ -1,41 +1,23 @@
--- Доказательство фундирования для Mem - нестандартные элиминаторы
+-- Доказательство фундирования для Mem - внутренний индукц.принцип
+import Init.WF
 import SigmaProgramming.Defs
 
 namespace GES
 variable [m : SModel]
 open S SM
 
--- Определяем элиминаторы для S, SM
-@[induction_eliminator]
-def S.induction {motive : S → Sort u}
-  (nil : motive nil)
-  (cons : (tl : S) → (hd : SM) → motive tl → motive (tl ∷ hd))
-  : (x : S) → motive x
-    | .nil => nil
-    | .cons tl hd => cons tl hd (induction nil cons tl)
+#print SM.rec
 
-@[induction_eliminator]
-def SM.induction {motive : SM → Sort u}
-  (atom : (a : M)  → motive (atom a))
-  (listn : motive (list nil))
-  (listc : (tl : S) → (hd : SM) → motive (list tl) → motive hd
-    → motive (list (tl ∷ hd)))
-  : (α : SM) → motive α
-    | .atom a => atom a
-    | .list nil => listn
-    | .list (tl ∷ hd) =>
-      listc tl hd (induction atom listn listc (list tl))
-                  (induction atom listn listc hd)
-
-theorem mem_wf : WellFounded Mem := by
+theorem mem_wf : WellFounded SM.Mem := by
   constructor
-  intro s
-  induction s with
-  | atom _ => constructor; intros; contradiction
-  | listn => constructor; intros; contradiction
-  | listc _ _ ih ih2 =>
-    constructor
-    intro _ p
-    cases p with
-    | here => exact ih2
-    | there hh => exact Acc.inv ih hh
+  intro sm
+  apply SM.rec
+    (motive_1 := λ s ↦ ∀ x ∈ (list s), Acc Mem x)
+    (motive_2 := λ sm ↦ Acc Mem sm)
+  · nofun                                     -- nil
+  · intro _ _ ih1 ih2 _ h                     -- cons
+    cases h with
+    | here => apply ih2
+    | there el => apply ih1 _ el
+  · intros; constructor; intro _ h; cases h   -- atom
+  · intro _ ih; constructor; apply ih         -- list
